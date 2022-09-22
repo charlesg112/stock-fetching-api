ed918e1df33cbce42117b8fc19abaf1d08cf735b
charlesg112 <charles.antoine.giroux42@gmail.com>
2022-09-22 17:33:21 -0400

    in memory repo wip

---
+++ test/persistence/memory/in-memory-stock-repository.test.ts
@@ -0,0 +1,7 @@
+describe("Memory Stock Repository", () => {
+    const repository = new MemoryStockRepository();
+
+    test("Given saved stocks When finding all stocks Then all stocks are returned", () => {
+
+    })
+})
